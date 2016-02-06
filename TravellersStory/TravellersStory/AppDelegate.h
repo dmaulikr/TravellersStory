869fdb4ef5d064da2f25126ce90e4f5ca1672db2
Stella Valcheva <stellavalcheva@yahoo.com>
2016-02-06 23:03:58 +0200

    CoreData configuration

--- TravellersStory/TravellersStory/AppDelegate.h
+++ TravellersStory/TravellersStory/AppDelegate.h
@@ -12,6 +12,12 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (nonatomic, strong, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, strong, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+-(void)saveContext;
 
 @end
 
